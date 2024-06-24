b98f5957ec4ab3a29d504c80f8c22a4b3d6db328
Kursat Keskin <kursatkeskinn@gmail.com>
2024-06-24 11:30:26 +0100

    place holder color changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        customGray: "#80868B",
+        customGray: "#BDC1C6",
       },
       screens: {
         md: "300px", // Set the sm breakpoint to 425px
