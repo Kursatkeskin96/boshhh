b9cbcedbca36c14ae1318203ff846a9d677fbf3e
Kursatkeskin96 <kursatkeskinn@gmail.com>
2024-06-26 10:31:17 +0100

    paymentelement added

--- components/Stripe.jsx
+++ components/Stripe.jsx
@@ -1,7 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
-import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { Elements, PaymentElement, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
