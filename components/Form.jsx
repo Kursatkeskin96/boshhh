8bb0d0c0122a888d7660403bd7e48caad56fda72
Kursatkeskin96 <kursatkeskinn@gmail.com>
2024-06-26 13:04:28 +0100

    remove loading

--- components/Form.jsx
+++ components/Form.jsx
@@ -16,7 +16,6 @@ export default function Form(props) {
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
   const [phone, setPhone] = useState("");
-  const [loading, setLoading] = useState("false")
   const [dateOfBirth, setDateOfBirth] = useState({
     day: "",
     month: "",
@@ -202,7 +201,6 @@ export default function Form(props) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true)
     const data = {
       title: "Mr",
       firstName: firstname,
@@ -238,11 +236,9 @@ export default function Form(props) {
 
       const result = await response.json();
       console.log("Success:", result);
-      setLoading(false)
       router.push(`/checkout?email=${encodeURIComponent(email)}`);
     } catch (error) {
       console.error("Error:", error);
-      setLoading(false)
     }
   };
 
@@ -489,7 +485,7 @@ export default function Form(props) {
             type="submit"
             className="bg-[#1E1E1E] flex justify-center items-center w-[90%] lg:w-[526px] h-[40px] py-15 pl-32 pr-24 mt-6 font-normal text-white rounded-[100px]"
           >
-           {!loading ? "loading..." : 'place order'}
+           place order
           </button>
       </form>
     </div>
