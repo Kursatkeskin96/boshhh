944d2088a77bc1bf0e18b048bbad6a5806d5fee9
Kursatkeskin96 <kursatkeskinn@gmail.com>
2024-06-26 10:11:03 +0100

    useffect added

--- components/Stripe.jsx
+++ components/Stripe.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
